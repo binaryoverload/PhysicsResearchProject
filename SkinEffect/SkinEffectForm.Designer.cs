a7ea3c679cb24f4f962afbb3ddaeb94a770c34ff
William Oldham <wegg7250@gmail.com>
2018-06-17 10:05:21 +0100

    Add project files.

---
+++ SkinEffect/SkinEffectForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class SkinEffectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.currentDensityTxtbox = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.skinDepthTxtbox = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.wireThicknessTxtbox = new System.Windows.Forms.ToolStripTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.AxisX.MajorGrid.Enabled = false;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisX.MinorTickMark.Enabled = true;
+            chartArea2.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.chart1.Location = new System.Drawing.Point(0, 28);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(749, 368);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.currentDensityTxtbox,
+            this.toolStripSeparator1,
+            this.toolStripLabel2,
+            this.skinDepthTxtbox,
+            this.toolStripSeparator2,
+            this.toolStripLabel3,
+            this.wireThicknessTxtbox});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(749, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(127, 22);
+            this.toolStripLabel1.Text = "Initial Current Density: ";
+            // 
+            // currentDensityTxtbox
+            // 
+            this.currentDensityTxtbox.Name = "currentDensityTxtbox";
+            this.currentDensityTxtbox.Size = new System.Drawing.Size(100, 25);
+            this.currentDensityTxtbox.Text = "100";
+            this.currentDensityTxtbox.TextChanged += new System.EventHandler(this.currentDensityTxtbox_TextChanged);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(70, 22);
+            this.toolStripLabel2.Text = "Skin Depth: ";
+            // 
+            // skinDepthTxtbox
+            // 
+            this.skinDepthTxtbox.Name = "skinDepthTxtbox";
+            this.skinDepthTxtbox.Size = new System.Drawing.Size(100, 25);
+            this.skinDepthTxtbox.Text = "0.05";
+            this.skinDepthTxtbox.TextChanged += new System.EventHandler(this.skinDepthTxtbox_TextChanged);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(108, 22);
+            this.toolStripLabel3.Text = "Thickness Of Wire: ";
+            // 
+            // wireThicknessTxtbox
+            // 
+            this.wireThicknessTxtbox.Name = "wireThicknessTxtbox";
+            this.wireThicknessTxtbox.Size = new System.Drawing.Size(100, 25);
+            this.wireThicknessTxtbox.Text = "10";
+            this.wireThicknessTxtbox.TextChanged += new System.EventHandler(this.wireThicknessTxtbox_TextChanged);
+            // 
+            // SkinEffectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 396);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.chart1);
+            this.Name = "SkinEffectForm";
+            this.Text = "Skin Effect";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.SkinEffectForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox currentDensityTxtbox;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox skinDepthTxtbox;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripTextBox wireThicknessTxtbox;
+    }
+}
+
---
+++ SkinEffect/SkinEffectForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp2
+{
+    public partial class SkinEffectForm : Form
+    {
+
+        Point? prevPosition = null;
+        ToolTip tooltip = new ToolTip();
+
+        double initalCurrentDensity;
+        double skinDepth;
+        double wireThickness;
+
+        public SkinEffectForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].CursorX.LineColor = Color.Aqua;
+            chart1.MouseMove += new MouseEventHandler(chart1_MouseMove);
+            currentDensityTxtbox.KeyPress += new KeyPressEventHandler(numberonly_txtbox);
+            skinDepthTxtbox.KeyPress += new KeyPressEventHandler(numberonly_txtbox);
+            wireThicknessTxtbox.KeyPress += new KeyPressEventHandler(numberonly_txtbox);
+
+
+            initalCurrentDensity = Double.Parse(currentDensityTxtbox.Text);
+            skinDepth = Double.Parse(skinDepthTxtbox.Text);
+            wireThickness = Double.Parse(wireThicknessTxtbox.Text);
+
+            updateGraph();
+            adjustSizes();
+        }
+
+
+        private double calculateCurrentDensity(double initialCurrent, double depth, double skinDepth)
+        {
+            return initialCurrent * Math.Pow(Math.E, -depth / skinDepth);
+        }
+
+        private void updateGraph()
+        {
+            chart1.Series[0].Points.Clear();
+            for (double depth = 0; depth <= wireThickness / 2; depth += (wireThickness / 200))
+            {
+                chart1.Series[0].Points.AddXY(depth, calculateCurrentDensity(initalCurrentDensity, depth, skinDepth));
+          
+            }
+            chart1.ChartAreas[0].CursorY.Position = initalCurrentDensity * Math.Pow(Math.E, -1);
+            chart1.ChartAreas[0].AxisY.Maximum = initalCurrentDensity;
+        }
+
+        private void numberonly_txtbox(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as ToolStripTextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void chart1_MouseMove(object sender, MouseEventArgs e)
+        {
+            var pos = e.Location;
+            if (prevPosition.HasValue && pos == prevPosition.Value)
+                return;
+            tooltip.RemoveAll();
+            prevPosition = pos;
+            var results = chart1.HitTest(pos.X, pos.Y, false,
+                                            ChartElementType.DataPoint);
+            foreach (var result in results)
+            {
+                if (result.ChartElementType == ChartElementType.DataPoint)
+                {
+                    var prop = result.Object as DataPoint;
+                    if (prop != null)
+                    {
+                        var pointXPixel = result.ChartArea.AxisX.ValueToPixelPosition(prop.XValue);
+                        var pointYPixel = result.ChartArea.AxisY.ValueToPixelPosition(prop.YValues[0]);
+
+                        // check if the cursor is really close to the point (2 pixels around the point)
+                        if (Math.Abs(pos.X - pointXPixel) < 50 &&
+                            Math.Abs(pos.Y - pointYPixel) < 50)
+                        {
+                            tooltip.Show("X=" + prop.XValue + ", Y=" + prop.YValues[0], this.chart1,
+                                            pos.X, pos.Y - 15);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void currentDensityTxtbox_TextChanged(object sender, EventArgs e)
+        {
+            initalCurrentDensity = Double.Parse((sender as ToolStripTextBox).Text == "" ? "0" : (sender as ToolStripTextBox).Text);
+            updateGraph();
+        }
+
+        private void skinDepthTxtbox_TextChanged(object sender, EventArgs e)
+        {
+            skinDepth = Double.Parse((sender as ToolStripTextBox).Text == "" ? "0" : (sender as ToolStripTextBox).Text);
+            updateGraph();
+        }
+
+        private void wireThicknessTxtbox_TextChanged(object sender, EventArgs e)
+        {
+            wireThickness = Double.Parse((sender as ToolStripTextBox).Text == "" ? "0" : (sender as ToolStripTextBox).Text);
+            updateGraph();
+        }
+
+        private void SkinEffectForm_Resize(object sender, EventArgs e)
+        {
+            adjustSizes();
+        }
+
+        private void adjustSizes()
+        {
+            chart1.Height = ClientSize.Height - 25;
+        }
+    }
+}
